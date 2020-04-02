83ac3ab1ef6fce2ce7490b7d27e6d796832f5f78
Saif Minhas <saif_minhas@hotmail.com>
2020-04-02 11:16:43 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -9,7 +9,13 @@ end
 
 def introduction_with_language(name, language)
   
-  puts " Hi, my an"
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
+
+def introduction_with_language_optional(name, language ="Ruby")
+  
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
 
   
 
